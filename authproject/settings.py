f2a1a6ba293134eb299b93325db87e71bcfeed16
Ashish Roul <56018558+ashish8763@users.noreply.github.com>
2021-04-06 10:35:28 +0530

    third commit

--- authproject/settings.py
+++ authproject/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'authproject.urls'
@@ -121,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL='/'#after login it will gose to home page
 LOGOUT_REDIRECT_URL='/logout'#after logout it will gose to home page insted of admin logout
+
+STATICFILES_STORAGE='whitenoise.storage.CompressManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
